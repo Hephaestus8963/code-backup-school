4c14f6530ff31bd8472f85b8d7c226508783b47d
Hephaestus8963 <84956200+Hephaestus8963@users.noreply.github.com>
2022-10-06 12:00:53 +0600

    start

---
+++ Modules/GameWindow.py
@@ -0,0 +1,6 @@
+import tkinter as TK
+
+class Window:
+
+    def __init__(height, width, title):
+        TK.Tk()
---
+++ index.py
@@ -0,0 +1,7 @@
+import tkinter as kin
+
+Window = kin.Tk("Test")
+
+Window.aspect(minNumer=100, maxNumer=100)
+
+Window.mainloop()
